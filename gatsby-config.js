ff2cc00162cad95a003b21ca406c6d73ddf9fdf6
rjiang98 <richard.jiang98@gmail.com>
2019-09-17 18:30:58 -0700

    Package jank

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,25 +7,25 @@ module.exports = {
     author: `Richard Jiang`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/favicon.png",
-
-        appName: null, // Inferred with your package.json
-        appDescription: null,
-        developerName: null,
-        developerURL: null,
-        dir: "auto",
-        lang: "en-US",
-        background: "#fff",
-        theme_color: "#fff",
-        display: "standalone",
-        orientation: "any",
-        start_url: "/?homescreen=1",
-        version: "1.0",
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-favicon`,
+    //   options: {
+    //     logo: "./src/favicon.png",
+    //
+    //     appName: null, // Inferred with your package.json
+    //     appDescription: null,
+    //     developerName: null,
+    //     developerURL: null,
+    //     dir: "auto",
+    //     lang: "en-US",
+    //     background: "#fff",
+    //     theme_color: "#fff",
+    //     display: "standalone",
+    //     orientation: "any",
+    //     start_url: "/?homescreen=1",
+    //     version: "1.0",
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -51,7 +51,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // 'gatsby-plugin-offline',
